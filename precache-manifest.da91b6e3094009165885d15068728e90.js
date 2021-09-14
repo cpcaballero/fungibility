79d8ce3e091eaf9dd502f1dfd06c3d0bb86d02fb
Carlos Caballero <carlospcaballero@outlook.com>
2021-09-14 13:56:46 +0800

    init

---
+++ precache-manifest.da91b6e3094009165885d15068728e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3acdbd03e57cd98fc6e48cc069b4351",
+    "url": "/fungibility/index.html"
+  },
+  {
+    "revision": "bb13f2642ee3e2e299f1",
+    "url": "/fungibility/static/css/2.fab18f34.chunk.css"
+  },
+  {
+    "revision": "4bc70ed57191c685f254",
+    "url": "/fungibility/static/css/main.10f47e5d.chunk.css"
+  },
+  {
+    "revision": "bb13f2642ee3e2e299f1",
+    "url": "/fungibility/static/js/2.91658e5c.chunk.js"
+  },
+  {
+    "revision": "a31bc19815ec44f2654df7e10dd3107b",
+    "url": "/fungibility/static/js/2.91658e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc70ed57191c685f254",
+    "url": "/fungibility/static/js/main.142bf8cb.chunk.js"
+  },
+  {
+    "revision": "99e4062b0b7d4c2f3526",
+    "url": "/fungibility/static/js/runtime-main.3f2e953a.js"
+  },
+  {
+    "revision": "47b1738f67ccbe0bcaf025c010360389",
+    "url": "/fungibility/static/media/logoX.47b1738f.png"
+  }
+]);
